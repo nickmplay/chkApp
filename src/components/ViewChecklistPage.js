85e1e3fbc0d7fe1c284e03ba2cb2813a176d0a1b
Nicholas Montagni <nickmontagni@hotmail.com>
2018-02-01 13:19:31 +0000

    amended viewCheckist styling

--- src/components/ViewChecklistPage.js
+++ src/components/ViewChecklistPage.js
@@ -2,16 +2,29 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-export const ViewChecklistPage = (props) => (
-  <div className="content-container">
-    <h1>Viewing {props.checklist[0].name}</h1>
-    {props.checklist[0].items.map( (e, i) => <p key={i}>{e}</p>)}
-    <Link to="/">Return to dashboard</Link>
-  </div>
-);
+
+export class ViewChecklistPage extends React.Component {
+  onToggle = (e) => {
+    if (e.target.className == 'chk-unselected') {
+      e.target.className = 'chk-selected';
+    } else {
+      e.target.className = 'chk-unselected';
+    }
+  }
+
+  render() {
+    return (
+      <div className="content-container">
+        <h1>Viewing {this.props.checklist[0].name}</h1>
+        {this.props.checklist[0].items.map((e, i) => <p className='chk-unselected' onClick={this.onToggle} key={i}> {e}</p>)}
+        <Link to="/">Return to dashboard</Link>
+      </div>
+    )
+  };
+};
 
 const mapStateToProps = (state, props) => ({
-  checklist: state.checklists.filter( e => e.id.toString() === props.match.params.id)
+  checklist: state.checklists.filter(e => e.id.toString() === props.match.params.id)
 });
 
 export default connect(mapStateToProps)(ViewChecklistPage);
--- src/tests/components/ViewChecklistPage.test.js
+++ src/tests/components/ViewChecklistPage.test.js
@@ -7,3 +7,27 @@ test('should render ViewChecklistPage correctly with data', () => {
   const wrapper = shallow(<ViewChecklistPage checklist={[checklists[0]]} />);
   expect(wrapper).toMatchSnapshot();
 });
+
+test('should change class upon clicking', () => {
+  const onToggle = jest.fn();
+  const targetMock = {
+    target: { className: 'chk-unselected' }
+  };
+  const wrapper = shallow(<ViewChecklistPage
+    onToggle={onToggle}
+    checklist={[checklists[0]]}
+  />);
+
+  expect(wrapper).toMatchSnapshot();
+  wrapper.find('p').at(0).simulate('click', targetMock);
+  expect(targetMock).toEqual({
+    target: { className: 'chk-selected' }
+  });
+  // expect(onToggle).toHaveBeenCalled();
+
+  wrapper.find('p').at(0).simulate('click', targetMock);
+  expect(targetMock).toEqual({
+    target: { className: 'chk-unselected' }
+  });
+  // expect(onToggle).toHaveBeenCalled();
+});
